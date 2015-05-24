393faeda54f51f2b0379844103d3fc5c18d939e6
anirudh-ra <anirudh.ra@gmail.com>
2015-05-24 14:14:36 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,11 +7,11 @@ makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                m <<- NULL ## assigning the input matrix(y) to a temporary matrix(x)
         }
         get <- function() {x}
-        setinverse <- function(inverse) {m <<- inverse}
-        getinverse <- function() {m}
+        setinverse <- function(inverse) {m <<- inverse} ## taking the inverse of matrix(x) and storing it in memory
+        getinverse <- function() {m} ## getting the inverse of matrix from memory 
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -24,12 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
  m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
+        if(!is.null(m)) { 
+                message("getting cached data")  ## checking if the cache already exists in the memory
                 return(m)
         }
-        data <- x$get()
-        m <- solve(data)
-        x$setinverse(m)
-        m
+        data <- x$get()  ## if invers3e already exists in memory, get that copy of the solved matrix
+        m <- solve(data) ## if it doesn't exist, solve it again
+        x$setinverse(m) 
+        m  ## return the inverse to the main program
 }
